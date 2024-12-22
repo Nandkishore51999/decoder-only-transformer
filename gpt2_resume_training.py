261aba494b11e73d829e51dbe226efab41148ab3
Nandkishore51999 <nandkishore51999@gmail.com>
2024-12-22 09:51:57 +0000

    Made same changes to resume training

--- gpt2_resume_training.py
+++ gpt2_resume_training.py
@@ -1,16 +1,33 @@
 from gpt2 import *
 import torch
 import time
+import argparse
+import nanoid
+import os
+
 device = 'cuda'
 
 model_t0 = time.time()
 
-train_loader = DataloaderLite(B=4, T=1024, data="train")  # optimal value for RTX 4070ti (12GM VRAM) > 6, 1024 > 4, 1024
-val_loader = DataloaderLite(B=4, T=1024, data="val")
+parser = argparse.ArgumentParser()
+parser.add_argument('--B', type=int, default=8)
+parser.add_argument('--T', type=int, default=1024)
+parser.add_argument('--max_steps', type=int, default=1_220_704)
+args = parser.parse_args()
 
+B = args.B  # Use the passed argument for the batch size
+T = args.T  # Use the passed argument for the batch size
+max_steps = args.max_steps  # Use the passed argument for the batch size
+
+# train_loader = DataloaderLite(B=4, T=1024, data="train")  # optimal value for RTX 4070ti (12GM VRAM) > 6, 1024 > 4, 1024
+# val_loader = DataloaderLite(B=4, T=1024, data="val")
+data_dir = "edu_fineweb10B"
+train_loader = DataLoaderNumpy(B=B, T=T, data="train", data_dir=data_dir)  # optimal value for RTX 4070ti (12GM VRAM) > 6, 1024 > 4, 1024
+val_loader = DataLoaderNumpy(B=B, T=T, data="val", data_dir=data_dir)
 
 model = GPT(GPTConfig())
 model.to(device)
+
 # model.load_state_dict(torch.load("C:\Workspace-ML\decoder-only-transformer\log\model_00099.pt", weights_only=True))
 # model.eval()
 optimizer = model.config_optimizer(weight_decay=0.1, learning_rate=6e-4, device=device)
@@ -23,10 +40,14 @@ def load_checkpoint(model, optimizer, filename='checkpoint.pth.tar'):
         print("=> loading checkpoint '{}'".format(filename))
         checkpoint = torch.load(filename)
         step = checkpoint['step']
-        model.load_state_dict(checkpoint['model'])
+        try:
+            model.load_state_dict(checkpoint['model'])
+        except:
+            new_state_dict = {key.replace("_orig_mod.", ""): value for key, value in checkpoint['model'].items()}
+            model.load_state_dict(new_state_dict)
         optimizer.load_state_dict(checkpoint['optimizer'])
-        # lr = 0.1*checkpoint['lr']
-        lr = 0.60006e-05
+        lr = 0.1*checkpoint['lr']
+        # lr = 0.60006e-05
         print("=> loaded checkpoint '{}' (step {})"
                   .format(filename, checkpoint['step']))
     else:
@@ -35,32 +56,35 @@ def load_checkpoint(model, optimizer, filename='checkpoint.pth.tar'):
     return model, optimizer, step, lr
 
 
-model, optimizer, step_done, lr = load_checkpoint(model, optimizer, filename="C:\Workspace-ML\decoder-only-transformer\log3\model_1497.pt")
-
+model, optimizer, step_done, lr = load_checkpoint(model, optimizer, filename="/home/nand-ml/decoder-only-transformer/log_1024_BlockSize_1220704_Steps_8_B_1024_T_tG2nwc/model_1000000.pt")
+model = torch.compile(model)
 
 # max_lr = optimizer.defaults['lr']
 # min_lr = max_lr * 0.1
 # warmup_steps = 10
-max_steps = step_done + 500
+max_steps = step_done + 500_000
 
-val_step_unit = 100
-model_ckpt_step_unit = 250
+val_step_unit = 1000
+model_ckpt_step_unit = [100_000]
 
-def get_lr(it):
-    # if it < warmup_steps:
-    #     return max_lr * (it+1) / warmup_steps
+# def get_lr(it):
+#     # if it < warmup_steps:
+#     #     return max_lr * (it+1) / warmup_steps
 
-    if it > max_steps:
-        return min_lr
+#     if it > max_steps:
+#         return min_lr
 
-    decay_ratio = (it-step_done)/max_steps
-    assert 0 <= decay_ratio <= 1
-    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))  # starts at 1 and goes to 0
-    return min_lr + coeff * (max_lr - min_lr)
+#     decay_ratio = (it-step_done)/max_steps
+#     assert 0 <= decay_ratio <= 1
+#     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))  # starts at 1 and goes to 0
+#     return min_lr + coeff * (max_lr - min_lr)
 
 
 # create the log directory we will write checkpoints to and log to
 log_dir = "log4" #todo
+log_dir = f"log_{nanoid.generate(size=6)}"
+
+
 os.makedirs(log_dir, exist_ok=True)
 log_file = os.path.join(log_dir, f"log.txt")
 with open(log_file, "w") as f: # open for writing to clear the file
@@ -72,7 +96,7 @@ for step in range(step_done, max_steps):
     last_step = (step == max_steps - 1)
 
     # once in a while evaluate our validation loss
-    if step % val_step_unit == 0 or last_step:
+    if step % val_step_unit == 0 or step in model_ckpt_step_unit or last_step:
         model.eval()
         # val_loader.reset()
         with torch.no_grad():
@@ -89,7 +113,7 @@ for step in range(step_done, max_steps):
         print(f"validation loss: {val_loss_accum.item():.4f}")
         with open(log_file, "a") as f:
             f.write(f"{step} val {val_loss_accum.item():.4f}\n")
-        if step > 0 and (step % model_ckpt_step_unit == 0 or last_step):
+        if step > 0 and (step in model_ckpt_step_unit or last_step):
             # optionally write model checkpoints
             checkpoint_path = os.path.join(log_dir, f"model_{step}.pt")
             checkpoint = {
@@ -126,10 +150,10 @@ for step in range(step_done, max_steps):
     optimizer.step()
     torch.cuda.synchronize()
     t2 = time.time()
-    dt = (t2-t1)*1000
+    dt = (t2-t1)
     tokens_per_sec = (train_loader.B * train_loader.T)/dt
     # if i % 10 == 0:
-    print(f"Step: {step} | Loss: {loss.item()} | Lr: {lr:0.4e} | norm: {norm:.4f} | dt: {dt:.2f}ms | tokens/sec: {tokens_per_sec:.2f}")  # loss.item() will convert the tensor to float, so it lives in CPU.
+    print(f"Step:{step}| Loss: {loss.item():.4f} | Lr: {lr:0.2e} | norm: {norm:.2f} | dt: {dt*1000:.2f}ms | t/s: {tokens_per_sec:.2f}")  # loss.item() will convert the tensor to float, so it lives in CPU.
     with open(log_file, "a") as f:
         f.write(f"{step} train {loss.item():.6f}\n")
         f.write(f"{step} lr {lr:0.4e}\n")
@@ -139,7 +163,7 @@ print("total_epochs: ", no_epochs)
 
 # at starting point (first loss value), when probability of each vocab in our vocabulary is equal due to random initialization,
 # expected loss ~ -ln(1/50257) ~10.8
-print(loss)
+# print(loss)
 
 model_save_path = os.path.join(log_dir, f"gpt2_124M_BT_4_1024_{max_steps}.pt")
 torch.save(model.state_dict(), model_save_path)
